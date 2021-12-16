3dc601daff55ce68c0fde51a0ea3d64afaa607b4
Asim Liaquat <asimlqt22@gmail.com>
2021-12-16 23:13:59 +0000

    Added php-fpm 7.4 and php-apache 8.1

--- build-push.sh
+++ build-push.sh
@@ -58,6 +58,15 @@ cd "${WORKDIR}/apache/8.0-dev/"
 docker build --no-cache -t asimlqt/php-apache:8.0-dev .
 docker push asimlqt/php-apache:8.0-dev
 
+docker pull php:8.1-apache
+cd "${WORKDIR}/apache/8.1/"
+docker build --no-cache -t asimlqt/php-apache:8.1 .
+docker push asimlqt/php-apache:8.1
+
+cd "${WORKDIR}/apache/8.1-dev/"
+docker build --no-cache -t asimlqt/php-apache:8.1-dev .
+docker push asimlqt/php-apache:8.1-dev
+
 docker pull php:5.6-fpm
 cd "${WORKDIR}/fpm/5.6/"
 docker build --no-cache -t asimlqt/php-fpm:5.6 .
@@ -93,6 +102,15 @@ docker push asimlqt/php-fpm:7.3
 cd "${WORKDIR}/fpm/7.3-dev/"
 docker build --no-cache -t asimlqt/php-fpm:7.3-dev .
 docker push asimlqt/php-fpm:7.3-dev
+
+docker pull php:7.4-fpm
+cd "${WORKDIR}/fpm/7.4/"
+docker build --no-cache -t asimlqt/php-fpm:7.4 .
+docker push asimlqt/php-fpm:7.4
+
+cd "${WORKDIR}/fpm/7.4-dev/"
+docker build --no-cache -t asimlqt/php-fpm:7.4-dev .
+docker push asimlqt/php-fpm:7.4-dev
 
 docker pull php:8.0-fpm
 cd "${WORKDIR}/fpm/8.0/"
